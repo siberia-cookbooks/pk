ba8232809a1c140ff4a1992a25da50ae40b1c339
Jacques Marneweck <jacques@powertrip.co.za>
2012-09-22 16:42:50 +0200

    Add supported operating systems to the meta data
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,10 @@ license          "All rights reserved"
 description      "Checks out pk from github and the Joyent pkgsrc bits"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
+
+%w{
+  opensolaris
+  smartos
+}.each do |os|
+  supports os
+end
