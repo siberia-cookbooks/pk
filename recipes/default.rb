4a96125ffc5206d0ae8cae5849d78bcb8ad02101
Jacques Marneweck <jacques@powertrip.co.za>
2012-09-30 19:27:18 +0000

    Rework to use subscibes rather than notifies
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,51 +23,57 @@ end
 execute "git-submodule-init" do
   command "cd /opt/pkgsrc && git submodule init && git submodule update"
   action :nothing
+  subscribes :run, "git[/opt/pkgsrc]", :immediately
 end
 
 execute "git-pk-remote-add-upstream" do
   command "cd /opt/pk && git remote add upstream git://github.com/mamash/pk.git"
   not_if {node[:pk][:repos][:pk] == "git://github.com/mamash/pk.git"}
   action :nothing
+  subscribes :run, "git[/opt/pk]", :immediately
 end
 
 execute "git-pkgsrc-remote-add-upstream" do
   command "cd /opt/pkgsrc && git remote add upstream git://github.com/joyent/pkgsrc.git"
   not_if {node[:pk][:repos][:pkgsrc] == "git://github.com/joyent/pkgsrc.git"}
   action :nothing
+  subscribes :run, "git[/opt/pkgsrc]", :immediately
 end
 
 execute "git-pkgsrc-joyent-remote-add-upstream" do
   command "cd /opt/pkgsrc/joyent && git remote add upstream git://github.com/joyent/pkgsrc-joyent.git"
   not_if {node[:pk][:repos][:pkgsrc] == "git://github.com/joyent/pkgsrc.git"}
+  subscribes :run, "git[/opt/pkgsrc]", :immediately
   action :nothing
 end
 
 execute "git-pkgsrc-wip-remote-add-upstream" do
   command "cd /opt/pkgsrc/wip && git remote add upstream git://github.com/joyent/pkgsrc-joyent.git"
   not_if {node[:pk][:repos][:pkgsrc] == "git://github.com/joyent/pkgsrc.git"}
+  subscribes :run, "git[/opt/pkgsrc]", :immediately
   action :nothing
 end
 
-execute "git-clone-pkgsrc" do
-  command "cd /opt && git clone -b #{node[:pk][:pkgsrc_release]} #{node[:pk][:repos][:pkgsrc]}"
+git "/opt/pkgsrc" do
+  repository "#{node[:pk][:repos][:pkgsrc]}"
+  reference "#{node[:pk][:pkgsrc_release]}"
+  user "root"
+  group "root"
+  action :checkout
   not_if "test -d /opt/pkgsrc"
-  notifies :run, "execute[git-submodule-init]", :immediately
-  notifies :run, "execute[git-pkgsrc-remote-add-upstream]", :immediately
-  notifies :run, "execute[git-pkgsrc-joyent-remote-add-upstream]", :immediately
-  notifies :run, "execute[git-pkgsrc-wip-remote-add-upstream]", :immediately
 end
 
-execute "git-clone-pk" do
+git "/opt/pk" do
+  repository "#{node[:pk][:repos][:pk]}"
   if node[:pk][:pkgsrc_release] == "pkgsrc_2012Q2"
-    pk_branch="master"
+    reference "master"
   else
-    pk_branch=node[:pk][:pkgsrc_release]
+    reference "#{node[:pk][:pkgsrc_release]}"
   end
-
-  command "cd /opt && git clone -b #{pk_branch} #{node[:pk][:repos][:pk]}"
+  user "root"
+  group "root"
+  action :checkout
   not_if "test -d /opt/pk"
-  notifies :run, "execute[git-pk-remote-add-upstream]", :immediately
 end
 
 cookbook_file "/opt/pk/bin/generate-pkg_summary" do
