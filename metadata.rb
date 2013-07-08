c7bf5bb42b1d997ec0710facf3a17136e19be8d3
Jacques Marneweck <jacques@powertrip.co.za>
2013-07-08 20:02:10 +0200

    Use the MIT License
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "pk"
 maintainer       "Jacques Marneweck"
 maintainer_email "jacques@powertrip.co.za"
-license          "All rights reserved"
+license          "MIT"
 description      "Checks out pk from github and the Joyent pkgsrc bits"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.6"
